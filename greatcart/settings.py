13b4c34b2a6e5ad6919ad309ee6bbad5f5b7f4f4
Alpha125-Hyphen <mra579342@gmail.com>
2026-06-30 23:46:02 +0530

    django authentication

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -133,3 +133,19 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configuration
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mr9720993@gmail.com'
+EMAIL_HOST_PASSWORD = 'ravqizqwltmcmabs'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
